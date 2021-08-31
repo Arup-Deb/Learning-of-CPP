db09c014c360ec7806fc6d2fbe9687e1f288ae68
Arup Deb <debarup20010@gmail.com>
2021-08-31 23:09:27 +0530

    Add files via upload

---
+++ Tut 11 (break and continue statement).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int c;
+/***********Break statement**************/
+    /*for (int c = 0; c < 40; c++)
+    {
+       cout<<c<<endl;  // incase of this line  3 is printed.
+       if (c==3)
+       {
+          break;
+       }
+    }
+    */
+   /* for (int c = 0; c < 40; c++)
+    {
+       
+       if (c==3)
+       {
+          break;
+       }
+       cout<<c<<endl; // incase of this line  3 is not printed.
+    }
+    */
+
+    /************ continue statement*****************/
+    //for (int c = 0; c < 40; c++)
+    //{
+       
+       //if (c==3)
+   //   {
+   //     continue;
+   //   }
+   //   cout<<c<<endl; // incase of this line  3 is not printed.
+   // }
+   // return 0;
+   //    }
+
