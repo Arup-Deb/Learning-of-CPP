6a8b02ed0752ac8a30231d0231d3bcb3fe23982e
Arup Deb <debarup20010@gmail.com>
2021-09-09 22:29:33 +0530

    Add files via upload

---
+++ Tut 37(Inheritance Syntax & Visibility Mode in C++).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+// Base Class
+class Employee
+{
+public:
+    int id;
+    float salary;
+    Employee(int inpId)
+    {
+        id = inpId;
+        salary = 34.0;
+    }
+    Employee() {}
+};
+ // Derived Class syntax
+/*
+class {{derived-class-name}} : {{visibility-mode}} {{base-class-name}}
+{
+    class members/methods/etc...
+}
+Note: 
+1. Default visibility mode is private.
+2. public visibility mode: public menbers of the base class becomes public members of the derived class.
+3. Private visibility mode: public members of the base class becomes private members of the derived class.
+4. private members are never inherited
+*/
+
+// Creating a Programmer class derived from Employee Base class
+class Programmer : public Employee
+{
+public:
+    int languageCode;
+    Programmer(int inpId)
+    {
+        id = inpId;
+        languageCode = 9;
+    }
+    void getData(){
+        cout<<id<<endl;
+    }
+};
+int main()
+{
+    Employee harry(1), rohan(2);
+    cout << harry.salary << endl;
+    cout << rohan.salary << endl;
+    Programmer skillF(10);
+    cout << skillF.languageCode<<endl;
+    cout << skillF.id<<endl;
+    skillF.getData();
+    return 0;
+}
